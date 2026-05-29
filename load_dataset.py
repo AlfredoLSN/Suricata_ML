d09d346742fb12f3924fdc2c02fb73f6d101cf58
Alfredo Lucas <alfredoliu14@live.com>
2026-05-29 10:53:51 -0300

    feat: update data exploration and loading process by adjusting execution counts and removing duplicate handling

--- load_dataset.py
+++ load_dataset.py
@@ -16,6 +16,4 @@ def load(filenames: list):
         dfs.append(df)
     
     dfs = pd.concat(dfs)
-    print("Quantidade de duplicatas antes de remover: ", dfs.duplicated().sum())
-    dfs.drop_duplicates(inplace=True)
     return dfs
